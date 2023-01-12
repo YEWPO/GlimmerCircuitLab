bb70a2196846bedc7a2d1e369c3e523a02ff5ac1
22050908-Meng Pei <2398696309@qq.com>
2023-01-12 14:13:11 +0800

    Mux 4-1 2bits

--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -2,7 +2,12 @@ import chisel3._
 
 class Top extends Module {
   val io = IO(new Bundle {
+    val x = Input(Vec(4, UInt(2.W)))
+    val y = Input(UInt(2.W))
+    val f = Output(UInt(2.W))
   })
+
+  io.f := io.x(io.y)
 }
 
 object Top extends App {
