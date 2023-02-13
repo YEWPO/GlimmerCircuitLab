cdbf190ad9b65f54ff9eb5ee6c288663adde982e
22050908-Meng Pei <2398696309@qq.com>
2023-02-13 18:48:06 +0800

    Status Machine

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -7,8 +7,50 @@ object top extends App {
 
 class top extends Module {
   val io = IO(new Bundle {
-    val out = Output(UInt(8.W))
+    val in = Input(Bool())
+    val out = Output(Bool())
+    val status = Output(UInt(4.W))
   })
 
-  io.out := RegNext(Cat(io.out(4) ^ io.out(3) ^ io.out(2) ^ io.out(0), io.out(7, 1)), 1.U(8.W))
+  val statusReg = RegInit(0.U(4.W))
+  val outReg = RegInit(false.B)
+
+  io.status := statusReg
+  io.out := outReg
+  
+  when (io.in) {
+    when (statusReg === 0.U || statusReg === 1.U || statusReg === 2.U || statusReg === 3.U || statusReg === 4.U) {
+      statusReg := 5.U
+      outReg := false.B
+    } .elsewhen (statusReg === 5.U) {
+      statusReg := 6.U
+      outReg := false.B
+    } .elsewhen (statusReg === 6.U) {
+      statusReg := 7.U
+      outReg := false.B
+    } .elsewhen (statusReg === 7.U) {
+      statusReg := 8.U
+      outReg := true.B
+    } .elsewhen (statusReg === 8.U) {
+      statusReg := 8.U
+      outReg := true.B
+    }
+  } .otherwise {
+    when (statusReg === 0.U || statusReg === 5.U || statusReg === 6.U || statusReg === 7.U || statusReg === 8.U) {
+      statusReg := 1.U
+      outReg := false.B
+    } .elsewhen (statusReg === 1.U) {
+      statusReg := 2.U
+      outReg := false.B
+    } .elsewhen (statusReg === 2.U) {
+      statusReg := 3.U
+      outReg := false.B
+    } .elsewhen (statusReg === 3.U) {
+      statusReg := 4.U
+      outReg := true.B
+    } .elsewhen (statusReg === 4.U) {
+      statusReg := 4.U
+      outReg :=  true.B
+    }
+  }
 }
